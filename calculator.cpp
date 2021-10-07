68b4a1f68cc91fc7083a66b09247238ae92c5a6e
Di-peep <vdude112358@gmail.com>
2021-10-07 16:30:30 +0300

    Add some text

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,13 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+/*
+Lorem ipsum dolor sit amet consectetur adipiscing elit auctor dui,
+sapien magna montes nullam etiam venenatis condimentum laoreet,
+dis mattis volutpat elementum id interdum gravida efficitur.
+Ex rhoncus a tempor turpis primis orci sit dolor sem elit, magnis ipsum risus odio sapien efficitur cras conubia mattis fusce,
+eros hendrerit ultrices fames lobortis felis mauris consequat nulla. Taciti integer eleifend nisi sit amet in proin tristique,
+ante tempor duis parturient lacus vitae natoque scelerisque montes,
+iaculis magnis suscipit hendrerit tincidunt condimentum ac.
+*/
